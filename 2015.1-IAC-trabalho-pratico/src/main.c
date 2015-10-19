27ee47b186d8c099905546a118bc2a9e67094f7a
geraldohenrique <ghsmelo@gmail.com>
2015-10-19 20:33:39 -0200

    Teste da medição do tempo de CPU e da temporização

--- 2015.1-IAC-trabalho-pratico/src/main.c
+++ 2015.1-IAC-trabalho-pratico/src/main.c
@@ -1,33 +1,56 @@
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include <stdio.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+#include <times.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[], char *envp[]) {
+int main () {
 
 int pid ; /* identificador de processo */
+int tempo = 10;
+char medir;
+time_t horainicio;
+struct tms tempodecpu;
+
+medir = 'c';
 
 pid = fork () ; /* replicação do processo */
 
 if ( pid < 0 ) { /* se fork não funcionou */
 	perror ("Erro: ") ;
-	exit (-1) ; /* encerra o processo com código de erro -1 */ 
+	exit (-1) ; /* encerra o processo com código de erro -1 */
 }
-else if( pid > 0 ) /* se sou o processo pai*/ 
+else if( pid > 0 ) /* se sou o processo pai*/
 {
-	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-	//TODO após 10 segundos de execução, mate o proceso filho
+    horainicio = time(NULL);
+    while (difftime (time(NULL), horainicio) < tempo) {
+       printf("Child process user time = %fn",((double) tempodecpu.tms_cutime)/CLK_TCK);
+       sleep (1);
+    }
+
 }
-else /* senão, sou o processo filho*/ 
+else /* senão, sou o processo filho*/
 {
-	//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória:
+        if medir == 'c' {
+
+            for (;;){
+
+            }
+        }
+        if medir == 'm' {
+
+            for (;;){
+                 malloc(sizeof(100000));
+            }
+        }
 
 }
 perror ("Erro: ") ; /* execve não funcionou */
 
 printf ("Tchau !\n") ;
-exit(0) ; /* encerra o processo com sucesso (código 0) */ 
+exit(0) ; /* encerra o processo com sucesso (código 0) */
 
 }
